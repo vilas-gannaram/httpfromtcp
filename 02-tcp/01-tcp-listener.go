62998fb5bbce989ef5fb439a53e0266881bf0081
Vilas Gannaram <107317825+vilas-gannaram@users.noreply.github.com>
2025-11-26 15:10:32 +0530

    Implement TCP listener to read lines from connection

---
+++ 02-tcp/01-tcp-listener.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func getLines(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+
+		}
+
+		if str != "" {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	for line := range getLines(conn) {
+		fmt.Println(line)
+	}
+}
